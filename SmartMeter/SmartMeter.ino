7c327dbc3fd81cab0a326651ce6a22605f87a16b
kend94 <nandor.k94.knn@gmail.com>
2018-12-27 16:00:56 +0100

    Initial commit
    Added some files for proposed modules.

---
+++ SmartMeter/SmartMeter.ino
@@ -0,0 +1,14 @@
+/*
+Publishing online the consumption of a metering device with an esp 32 Board.
+
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
